308f736f6419099059cbd51008ff724b1e6e1679
Alejandro A <alejandroalejo714@gmail.com>
2025-07-22 17:21:47 -0600

    Initial Commit

---
+++ SimaApp/SimaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimaAppApp.swift
+//  SimaApp
+//
+//  Created by Alejandro Alejo on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SimaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
